a6d9f2a5cf7128f86fa435c4e57e03c534d1b7f8
Elliot Chow <chow.elliot@gmail.com>
2020-11-30 17:34:34 -0800

    init

---
+++ aggregate.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+tmpfile=`mktemp /tmp/XXXXX`
+out="aggregated-blacklist.txt"
+
+echo "tmpfile" $tmpfile
+
+echo "#### " `date` > $tmpfile
+
+for url in "https://raw.githubusercontent.com/r-a-y/mobile-hosts/master/AdguardApps.txt" \
+        "https://raw.githubusercontent.com/r-a-y/mobile-hosts/master/AdguardDNS.txt" \
+        "https://raw.githubusercontent.com/r-a-y/mobile-hosts/master/AdguardMobileAds.txt" \
+        "https://raw.githubusercontent.com/r-a-y/mobile-hosts/master/AdguardMobileSpyware.txt" \
+        "https://raw.githubusercontent.com/r-a-y/mobile-hosts/master/AdguardTracking.txt" \
+        "https://raw.githubusercontent.com/r-a-y/mobile-hosts/master/EasyPrivacy3rdParty.txt" \
+        "https://raw.githubusercontent.com/r-a-y/mobile-hosts/master/EasyPrivacySpecific.txt"
+do
+    echo "fetching" $url
+
+    curl --fail $url >> $tmpfile
+done
+
+for fname in blacklist.txt
+do
+    cat fname >> $tmpfile
+done
+
+mv $tmpfile $out
+
+git add $out
+
+git commit -m "update $out"
